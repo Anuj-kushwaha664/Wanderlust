003bde5f53cbe420f49e1c596c5095e20ac4d6e7
Anuj <anujkushwaha664@gmail.com>
2024-10-18 14:26:58 +0530

    index.ejs is styled and edit, show, new, footer and navbar page created and styled. Layout is created and incorporate with every page using EJS-MATE

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
-
+const methodOverride = require("method-override");
+const { redirect } = require("express/lib/response");
+const ejsMate = require("ejs-mate");
 
 // connection with database
 main().then(()=>{
@@ -18,30 +20,68 @@ async function main(){
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}))  // to enable url parsing
+app.use(methodOverride("_method"));
+app.engine("ejs",  ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 // server listening at route "/"
 app.get("/", (req,res)=>{
     res.send("hi, I am root");
 });
 
-app.get("/listing", async(req,res)=>{
+app.get("/listings", async(req,res)=>{
     const allListings = await Listing.find({});
     res.render("./Listings/index.ejs", {allListings});
 })
 
-app.get("/testListing", async(req, res)=>{
-    let sampleListing = new Listing({
-        title : "my new villa",
-        description : "By the beach",
-        price : 1200,
-        location: "Calangute, Goa",
-        country : "India"
-    })
+app.get("/listings/new", (req,res)=>{
+    res.render("./Listings/new.ejs");
+})
 
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful testing");
-});
+app.post("/listings", async(req,res)=>{
+    const newlisting = new Listing(req.body);
+    newlisting.save();
+    res.redirect("/listings");
+})
+app.get("/listings/:id",  async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    // console.log(listing);
+    res.render("./Listings/show.ejs", {listing});
+})
+
+app.get("/listings/:id/edit", async(req,res)=>{
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./Listings/edit.ejs", {listing});
+})
+
+app.put("/listings/:id", async(req,res)=>{
+    const {id} = req.params;
+    // console.log(id);
+   await Listing.findByIdAndUpdate(id, req.body);
+   res.redirect("/listings");
+})
+
+app.delete("/listings/:id", async(req,res)=>{
+    const {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+})
+// app.get("/testListing", async(req, res)=>{
+//     let sampleListing = new Listing({
+//         title : "my new villa",
+//         description : "By the beach",
+//         price : 1200,
+//         location: "Calangute, Goa",
+//         country : "India"
+//     })
+
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful testing");
+// });
 
 // starting server
 app.listen(8080, ()=>{
